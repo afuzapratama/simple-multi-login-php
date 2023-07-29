79ad16c6853a67a7137ea31200ecc837a679c139
afuzapratama <afuzapratama@yahoo.com>
2023-07-30 04:19:05 +0700

    bug:fix header

--- dashboard/success-reset.php
+++ dashboard/success-reset.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Password Reset</title>
-</head>
-
-<body>
-    <h1>Reset Password</h1>
+<?php
+echo  "<h1>Reset Password</h1>
     <p>
-        You New Password is: <?= $_GET['pass']  ?>
-    </p>
-</body>
-
-</html>
+        You New Password is: " . $_GET['pass'] . "
+    </p>";
